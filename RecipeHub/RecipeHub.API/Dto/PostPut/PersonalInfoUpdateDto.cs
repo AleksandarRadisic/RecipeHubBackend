0c9b1abfc7c9f7d1ae9ca47c8e25d0b702c33bb5
Aleksandar Radišić <73358934+AleksandarRadisic@users.noreply.github.com>
2022-09-14 17:49:08 +0200

    Update PersonalInfoUpdateDto.cs

--- RecipeHub/RecipeHub.API/Dto/PostPut/PersonalInfoUpdateDto.cs
+++ RecipeHub/RecipeHub.API/Dto/PostPut/PersonalInfoUpdateDto.cs
@@ -1,6 +1,14 @@
-﻿namespace RecipeHub.API.Dto.PostPut
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeHub.API.Dto.PostPut
 {
     public class PersonalInfoUpdateDto
     {
+        [Required(ErrorMessage = "Fist name is required")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last name is required")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Username is required")]
+        public string UserName { get; set; }
     }
 }
